618e84ea1955b369fcda329c72f8b993324cfcb5
Federico Aloi <federico.aloi@gmail.com>
2022-01-26 10:46:20 -0300

    Corregidas URLs OG

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -4,7 +4,11 @@ import { useStaticQuery } from "gatsby";
 import { graphql } from "gatsby";
 
 const SEO = ({ title, description, imagePath, path = "" }) => {
-  const data = useStaticQuery(graphql`
+  const {
+    site: {
+      siteMetadata: { siteUrl, title: siteTitle },
+    },
+  } = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
@@ -13,20 +17,20 @@ const SEO = ({ title, description, imagePath, path = "" }) => {
       }
     }
   `);
-  const fullUrl = (path: string) => `${data.site.siteMetadata.siteUrl}/${path}`;
+
   return (
     <Helmet>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:title" content={title}></meta>
       <meta name="twitter:description" content={description}></meta>
-      <meta name="twitter:image" content={fullUrl(imagePath)}></meta>
+      <meta name="twitter:image" content={siteUrl + imagePath}></meta>
       <meta property="og:locale" content="es_AR" />
-      <meta property="og:site_name" content={data.site.siteMetadata.title} />
+      <meta property="og:site_name" content={siteTitle} />
       <meta property="og:title" content={title}></meta>
       <meta property="og:type" content="article"></meta>
-      <meta property="og:url" content={fullUrl(path)}></meta>
+      <meta property="og:url" content={`${siteUrl}/${path}`}></meta>
       <meta property="og:description" content={description}></meta>
-      <meta property="og:image" content={fullUrl(imagePath)}></meta>
+      <meta property="og:image" content={siteUrl + imagePath}></meta>
     </Helmet>
   );
 };
